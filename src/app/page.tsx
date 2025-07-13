b76fab9d631565a4efb55e1683351c403be936ec
rafeieamirhosein <rafeieamirhosein@gmail.com>
2025-07-13 16:33:44 +0800

    Add input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+import Input from "@/components/Input";
 import styles from "./page.module.scss";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
-  return <div className={styles.container}>hello coinersho</div>;
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handlePhoneNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Input value={phoneNumber} onChange={handlePhoneNumberChange} />
+    </div>
+  );
 }
